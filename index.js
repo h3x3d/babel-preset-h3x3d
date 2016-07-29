0d09c98de775c10253d2dc87093bb4ea51c5d061
h3x3d <h3x3d@ya.ru>
2016-07-29 17:15:19 +0300

    Update && add error extending

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   ],
   plugins: [
     require('babel-plugin-transform-runtime'),
-    require('babel-plugin-transform-class-properties')
+    require('babel-plugin-transform-class-properties'),
+    [require('babel-plugin-transform-builtin-extend'), {globals: 'Error'}]
   ]
 };
