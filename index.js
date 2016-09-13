f87c6f277214d5eaf72532c2ebfa8dafb896d570
h3x3d <h3x3d@ya.ru>
2016-09-13 19:39:38 +0300

    async/await support

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ module.exports = {
     require('babel-preset-node6')
   ],
   plugins: [
-    require('babel-plugin-transform-runtime'),
+    require('babel-plugin-syntax-async-functions'),
     require('babel-plugin-transform-class-properties'),
-    [require('babel-plugin-transform-builtin-extend').default, { globals: ['Error'] }]
+    [require('babel-plugin-transform-builtin-extend').default, { globals: ['Error'] }],
+    require('babel-plugin-transform-async-to-generator'),
+    require('babel-plugin-transform-runtime'),
   ]
 };
