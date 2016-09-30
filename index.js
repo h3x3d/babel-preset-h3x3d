95108531e6da0f09d8e3802306b75eae70618abb
h3x3d <h3x3d@ya.ru>
2016-09-30 08:49:05 +0300

    Small cleanup

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = {
     require('babel-preset-react')
   ],
   plugins: [
-    require('babel-plugin-syntax-async-functions'),
     require('babel-plugin-transform-class-properties'),
     [require('babel-plugin-transform-builtin-extend').default, { globals: ['Error'] }],
     require('babel-plugin-transform-async-to-generator'),
